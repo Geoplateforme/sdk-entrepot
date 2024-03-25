5dc6a78857deb6deededde6b1a50a8bc09293429
Valentin Sasyan <valentin.sasyan@ign.fr>
2024-03-25 14:32:54 +0100

    AbstractResolver : get retourne un json si non str + message si clef non trouvée

--- tests/workflow/resolver/AbstractResolverTestCase.py
+++ tests/workflow/resolver/AbstractResolverTestCase.py
@@ -1,3 +1,6 @@
+from unittest.mock import MagicMock, PropertyMock, patch
+
+from sdk_entrepot_gpf.io.Config import Config
 from sdk_entrepot_gpf.workflow.resolver.AbstractResolver import AbstractResolver
 
 from tests.GpfTestCase import GpfTestCase
@@ -9,8 +12,8 @@ class AbstractResolverTestCase(GpfTestCase):
     cmd : python3 -m unittest -b tests.workflow.resolver.AbstractResolverTestCase
     """
 
-    def test_get(self) -> None:
-        """test unitaire pour get()"""
+    def test_get_ok(self) -> None:
+        """test unitaire pour get() quand ça fonctionne"""
 
         d_data = {"key": "val", "dict": {"key": "val_dict"}, "list": ["val_list", {"key": "val_list_dict"}]}
         l_res = [
@@ -21,3 +24,25 @@ class AbstractResolverTestCase(GpfTestCase):
         ]
         for s_js_key, s_res in l_res:
             self.assertEqual(s_res, AbstractResolver.get(d_data, s_js_key))
+
+    def test_get_ko(self) -> None:
+        """test unitaire pour get() quand ça ne fonctionne pas"""
+
+        d_data = {"key": "val", "dict": {"key": "val_dict"}, "list": ["val_list", {"key": "val_list_dict"}]}
+        l_res = [
+            ("bad_key", ("bad_key", "bad_key", "key, dict, list")),
+            ("dict.bad_key", ("bad_key", "bad_key", "key")),
+            ("bad_list[0]", ("bad_list", "bad_list[0]", "key, dict, list")),
+            ("list[1].bad_key", ("bad_key", "bad_key", "key")),
+        ]
+        for s_js_key, s_params in l_res:
+            with self.assertRaises(KeyError) as o_key_error:
+                o_fake_om = MagicMock()
+                o_fake_om.error = MagicMock()
+                with patch.object(Config, "om", new_callable=PropertyMock, return_value=o_fake_om):
+                    AbstractResolver.get(d_data, s_js_key)
+            # Vérifications
+            # Key error doit être levée
+            self.assertEqual(o_key_error.exception.args[0], s_params[0])
+            # une erreur doit être logué
+            o_fake_om.error.assert_called_once_with(f"Impossible de résoudre la clef '{s_js_key}' : sous-clef '{s_params[1]}' non trouvée, clefs possibles à ce niveau : {s_params[2]}")
