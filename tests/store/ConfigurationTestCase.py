4153eda1c31f1a5656ecac8512442509576513ab
LudivineSchlegel <35892777+LudivineSchlegel@users.noreply.github.com>
2026-09-22 10:10:04 +0200

    Update tests/store/ConfigurationTestCase.py

--- tests/store/ConfigurationTestCase.py
+++ tests/store/ConfigurationTestCase.py
@@ -48,15 +48,16 @@ class ConfigurationTestCase(GpfTestCase):
         """Vérifie que api_list_offerings surcharge bien la liste des champs demandés quand l'utilisateur en précise une."""
 
         o_response = GpfTestCase.get_response(json=[{"_id": "offering_1"}])
+        l_fields = ["_id", "status"]
 
         with patch.object(ApiRequester, "route_request", return_value=o_response) as o_mock_request:
             o_configuration = Configuration({"_id": "123456789"}, "id_datastore")
-            o_configuration.api_list_offerings(fields=["_id", "status"])
+            o_configuration.api_list_offerings(fields=l_fields)
             o_mock_request.assert_called_once_with(
                 "configuration_list_offerings",
                 route_params={"datastore": "id_datastore", "configuration": "123456789"},
                 method=ApiRequester.GET,
-                params={"fields": ["_id", "status"]},
+                params={"fields": l_fields},
             )
 
     def test_list_offerings_with_none_fields(self) -> None:
